4a9eddcf5a387e3a75869c86f498d2bbf3d2c086
Matthew Brush <matthewbrush(at)gmail(dot)com>
2011-08-01 20:43:34 -0700

    Make Python console go into message window notebook.

--- src/plugin.c
+++ src/plugin.c
@@ -5,6 +5,8 @@
 #include <dlfcn.h>
 #include <glib.h>
 #include <gtk/gtk.h>
+#include <pygobject.h>
+#include <pygtk/pygtk.h>
 #include <geanyplugin.h>
 
 #include "plugin-config.h"
@@ -16,7 +18,7 @@ GeanyData		*geany_data;
 GeanyFunctions	*geany_functions;
 
 
-PLUGIN_VERSION_CHECK(200)
+PLUGIN_VERSION_CHECK(211)
 
 PLUGIN_SET_INFO(_("GeanyPy"),
 				_("Python plugins support"),
@@ -25,7 +27,7 @@ PLUGIN_SET_INFO(_("GeanyPy"),
 
 
 static void
-GeanyPy_start_interpreter(const gchar *path_to_add)
+GeanyPy_start_interpreter(void)
 {
     gchar *init_code;
 
@@ -38,12 +40,7 @@ GeanyPy_start_interpreter(const gchar *path_to_add)
 
     Py_Initialize();
 
-    PyRun_SimpleString("import os,sys");
-    init_code = g_strdup_printf(
-        "sys.path.append('%s'.replace('~', os.path.expanduser('~')))",
-        path_to_add);
-    PyRun_SimpleString(init_code);
-    g_free(init_code);
+    /* Import the C modules */
     init_geany_dialogs();
     init_geany_filetype();
     init_geany_document();
@@ -55,7 +52,17 @@ GeanyPy_start_interpreter(const gchar *path_to_add)
     init_geany_file_prefs();
     init_geany_main_widgets();
     init_geany_scintilla();
-    PyRun_SimpleString("import geany");
+
+    /* Adjust Python path to find wrapper package (geany) */
+    init_code = g_strdup_printf(
+        "import os, sys\n"
+        "path = '%s'.replace('~', os.path.expanduser('~'))\n"
+        "sys.path.append(path)\n"
+        "import geany",
+        GEANYPY_PYTHON_DIR);
+    PyRun_SimpleString(init_code);
+    g_free(init_code);
+
 }
 
 static void
@@ -67,23 +74,83 @@ GeanyPy_stop_interpreter(void)
 
 
 static void
-GeanyPy_start_console(void)
+GeanyPy_install_console(void)
 {
-    if (Py_IsInitialized())
-        PyRun_SimpleString("geany.show_console()");
+    PyObject *module, *console, *console_inst, *args, *kwargs;
+    PyGObject *console_gobject;
+    GtkWidget *console_widget, *scroll;
+    PangoFontDescription *pfd;
+
+    module = PyImport_ImportModule("geany.console");
+    if (module == NULL)
+    {
+        g_warning("Failed to import console module");
+        return;
+    }
+
+    console = PyObject_GetAttrString(module, "Console");
+    Py_DECREF(module);
+
+    if (console == NULL)
+    {
+        g_warning("Failed to retrieve Console from console module");
+        return;
+    }
+
+    args = Py_BuildValue("()");
+    kwargs = Py_BuildValue("{s:s, s:s}",
+                "banner", "Geany Python Console",
+                "start_script", "import geany\n");
+
+    console_inst = PyObject_Call(console, args, kwargs);
+    Py_DECREF(console);
+    Py_DECREF(args);
+    Py_DECREF(kwargs);
+
+    if (console_inst == NULL)
+    {
+        g_warning("Unable to instantiate new Console");
+        return;
+    }
+
+    console_gobject = (PyGObject *) console_inst;
+    console_widget = GTK_WIDGET(console_gobject->obj);
+    Py_DECREF(console_inst);
+
+    if (console_widget == NULL)
+    {
+        g_warning("Failed to get GtkWidget for Console");
+        return;
+    }
+
+    pfd = pango_font_description_from_string("Monospace 9");
+    gtk_widget_modify_font(console_widget, pfd);
+    pango_font_description_free(pfd);
+
+    scroll = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll),
+        GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_container_add(GTK_CONTAINER(scroll), console_widget);
+
+    gtk_notebook_append_page(
+        GTK_NOTEBOOK(geany->main_widgets->message_window_notebook),
+        scroll,
+        gtk_label_new("Python"));
+
+    gtk_widget_show_all(scroll);
 }
 
 
 static void
 on_geany_startup_complete(GObject unused, gpointer user_data)
 {
-    GeanyPy_start_console();
+    GeanyPy_install_console();
 }
 
 
 void plugin_init(GeanyData *data)
 {
-	GeanyPy_start_interpreter(GEANYPY_PYTHON_DIR);
+	GeanyPy_start_interpreter();
 
     plugin_signal_connect(geany_plugin, NULL, "geany-startup-complete",
         TRUE, G_CALLBACK(on_geany_startup_complete), NULL);
