7ddd5c5d0a5710dd592fd7276beef2bdb9f75c2b
rob <robblue2x@gmail.com>
2015-07-11 06:54:10 +0100

    fix for  Geany Crashes if Geanypy is closed with the python console open #12 added call to deactivate all plugins from the C geany plugin to the python plugin manager, it is called from the geanypy's clean up function

--- src/geanypy-plugin.c
+++ src/geanypy-plugin.c
@@ -264,23 +264,25 @@ plugin_init(GeanyData *data)
         GeanyPy_init_manager(plugin_dir);
 
     loader_item = gtk_menu_item_new_with_label(_("Python Plugin Manager"));
-	gtk_widget_set_sensitive(loader_item, plugin_dir != NULL);
-	gtk_menu_append(GTK_MENU(geany->main_widgets->tools_menu), loader_item);
-	gtk_widget_show(loader_item);
-	g_signal_connect(loader_item, "activate",
-		G_CALLBACK(on_python_plugin_loader_activate), NULL);
+    gtk_widget_set_sensitive(loader_item, plugin_dir != NULL);
+    gtk_menu_append(GTK_MENU(geany->main_widgets->tools_menu), loader_item);
+    gtk_widget_show(loader_item);
+    g_signal_connect(loader_item, "activate",
+            G_CALLBACK(on_python_plugin_loader_activate), NULL);
 }
 
 G_MODULE_EXPORT void plugin_cleanup(void)
 {
-    PyObject* deactivate_all_plugins = PyObject_GetAttrString(manager, "deactivate_all_plugins");
-    if (deactivate_all_plugins == NULL)
+    PyObject* deactivate_all_plugins = PyObject_GetAttrString(manager,
+            "deactivate_all_plugins");
+    if (deactivate_all_plugins != NULL)
     {
-        g_warning(_("Unable to get deactivate_all_plugins() method on plugin manager"));
-        return;
+        PyObject* r = PyObject_CallObject(deactivate_all_plugins, NULL);
+        if (r)
+            Py_DECREF(r);
+        Py_DECREF(deactivate_all_plugins);
+
     }
-    PyObject_CallObject(deactivate_all_plugins, NULL);
-    Py_DECREF(deactivate_all_plugins);
 
     signal_manager_free(signal_manager);
     Py_XDECREF(manager);
